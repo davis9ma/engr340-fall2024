1ed8167d189dbe7470cf684b35509be3f62ee1c4
davis9ma <davis9ma@dukes.jmu.edu>
2024-08-28 09:47:59 -0400

    Davis dukes_unleashed.py

--- Module 1 - Language Intro/1 - Variables/4 - Assignments/1.1.4.1 - dukes/dukes_unleashed.py
+++ Module 1 - Language Intro/1 - Variables/4 - Assignments/1.1.4.1 - dukes/dukes_unleashed.py
@@ -10,6 +10,20 @@ Note: this problem does not require the "compounding interest" formula from the 
 
 ### Your code here ###
 
-in_state_gift = 0
+in_state_tuition = 13376 # Cost of in-state tuition & fees for 2022-2023
+out_of_state_tuition = 30466 # Cost of out-of-state tuition & fees for 2022-2023
+constant_fees = (5772+6268+1176+1968+2156+76) # Sum of fees for this period that remain constant
 
-out_state_gift = 0
+print("Constant fees for the year are $", constant_fees) # displays the constant fees
+print("In-State tuition and fees are $", in_state_tuition) # displays the in-state tuition value
+print("Out-of-State tuition and fees are $", out_of_state_tuition) # displays the out-of-state tuition value
+
+print() # prints an empty line for readability
+
+in_state_gift = in_state_tuition+constant_fees # sums the fees to get in-state gift value
+
+print("Total In-State gift value is $", in_state_gift) # displays in-state gift value
+
+out_state_gift = out_of_state_tuition+constant_fees # sums the fees to get out-of-state gift value
+
+print("Total Out-of-State gift value is $", out_state_gift) # displays the out-of-state gift value
